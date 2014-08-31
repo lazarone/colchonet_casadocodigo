90d19f5f6d7d265a46752560055d0c8e27a2bc5e
lazarone <lazarone@github.com>
2014-08-31 12:31:02 -0300

    criando o projeto ruby on rails colchonet

---
+++ colchonet/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Colchonet::Application.config.secret_key_base = '9e3288965aa02f24dd044b0bf918890277707174c9cf160e0d342d1232a333dcb163cbd1b4b32f19c50cdf025405f4b49b1903a3f87079d3a4b84187d3b41fa2'
