6e4a3eb14731b8593c891977b09c433d4c55b5cf
Santos.Lazarone <lazarone.info.web@gmail.com>
2014-10-11 19:23:51 -0300

    ajustes

--- colchonet/db/schema.rb
+++ colchonet/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141011212036) do
+ActiveRecord::Schema.define(version: 20141011221812) do
 
   create_table "rooms", force: true do |t|
     t.string   "title"
@@ -24,7 +24,6 @@ ActiveRecord::Schema.define(version: 20141011212036) do
   create_table "users", force: true do |t|
     t.string   "full_name"
     t.string   "email"
-    t.string   "password"
     t.string   "location"
     t.text     "bio"
     t.datetime "created_at"
