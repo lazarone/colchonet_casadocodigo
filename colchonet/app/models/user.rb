d27cc33801efe74121b174b5931f373b74c2bf87
Santos.Lazarone <lazarone.info.web@gmail.com>
2014-10-11 18:53:05 -0300

    ajustes password colocando criptografia"

--- colchonet/app/models/user.rb
+++ colchonet/app/models/user.rb
@@ -2,8 +2,8 @@ class User < ActiveRecord::Base
   EMAIL_REGEXP = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/
   
   validates_presence_of :email, :full_name, :location
-  validates_length_of :bio, :minimum: 30, allow_blank: false
-  validates_format_of :email, :with: EMAIL_REGEXP
+  validates_length_of :bio, :minimum => 30, :allow_blank => false
+  validates_format_of :email, :with => EMAIL_REGEXP
 
   has_secure_password
 end
